039f209ab01691a0f71f89be5ab121a68a53b2d9
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2017-02-16 16:59:09 +0800

    搭建开发环境

---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+describe('Test Default',()=>{
+  it("should always work",()=>{
+
+  });
+});
---
+++ tools/prestart.js
@@ -0,0 +1,3 @@
+import colors from 'colors';
+/* eslint-disable no-console */
+console.log('Start Development Envirment...'.green);
